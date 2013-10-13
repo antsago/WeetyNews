fbd3b6c5450802016d811ef29897c9151ce79704
Lamin Sanneh <lswebapps@gmail.com>
2013-10-14 00:15:27 +0100

    Deleted database file as its no longer needed
    Base model used to extend from this class but since it used PDO( which we are no longer using), we no longer need it

--- libs/database.php
+++ libs/database.php
@@ -1,10 +1,19 @@
-<?php  
+<?php 
 
-class Database extends PDO
+require_once 'php-activerecord/ActiveRecord.php';
+
+ActiveRecord\Config::initialize(function($cfg)
+{
+    $cfg->set_model_directory('../models');
+    $cfg->set_connections(array(
+        'development' => 'mysql://root:@localhost/WheetyNews'));
+});
+
+class Database extends ActiveRecord\Model
 {
 	
 	function __construct()
 	{
-		parent::__construct('mysql:host=localhost;dbname=WeetyNews', 'root', '');
+		// parent::__construct('mysql:host=localhost;dbname=WeetyNews', 'root', '');
 	}
 }
