2dec6cc45a2c891b2bbd5a47b8b7f3dc08294ffd
Lamin Sanneh <lswebapps@gmail.com>
2013-10-14 00:12:40 +0100

    Made the base model use active record instead of PDO

--- models/Model.php
+++ models/Model.php
@@ -1,12 +1,15 @@
 <?php
 
-require(__DIR__."/../libs/database.php");  
+require(__DIR__."/../libs/php-activerecord/ActiveRecord.php");  
 
-class Model
+ActiveRecord\Config::initialize(function($cfg)
+{
+    $cfg->set_model_directory(__DIR__.'/../models');
+    $cfg->set_connections(array(
+        'development' => 'mysql://root:@localhost/WeetyNews'));
+});
+
+class Model extends ActiveRecord\Model
 {
 	
-	function __construct()
-	{
-		$this->db = new Database();
-	}
 }
